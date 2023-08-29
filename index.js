683899f42e441f1ce4b9492a2b6ab7200a6213d3
dedricl <dedricleftwich@gmail.com>
2023-08-28 21:53:49 -0400

    code from html, css, and js

---
+++ index.js
@@ -0,0 +1,42 @@
+let homePoints = 0
+let guestPoints = 0
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+
+function addOneToHome(){
+    homePoints += 1
+    homeScore.textContent = homePoints
+}
+
+function addTwoToHome(){
+    homePoints += 2
+    homeScore.textContent = homePoints
+}
+
+function addThreeToHome(){
+    homePoints += 3
+    homeScore.textContent = homePoints
+}
+
+function addOneToGuest(){
+    guestPoints += 1
+    guestScore.textContent = guestPoints
+}
+
+function addTwoToGuest(){
+    guestPoints += 2
+    guestScore.textContent = guestPoints
+}
+
+function addThreeToGuest(){
+    guestPoints += 3
+    guestScore.textContent = guestPoints
+}
+
+function newGame() {
+    homePoints = 0
+    guestPoints = 0
+    homeScore.textContent = homePoints
+    guestScore.textContent = guestPoints
+}
